d9768af86ab9103cfdf6e344b8bb50a7f35833da
Radzio1976 <r.tomaszewski@gmail.com>
2023-03-15 10:29:03 +0100

    Moved usersIntegrators logic to different file usersIntegrators.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 const addUser = require('./httpRequests/addUser');
 const login = require('./httpRequests/login');
+const usersIntegrators = require('./httpRequests/usersIntegrators');
 
 const mongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://127.0.0.1:27017';
@@ -36,47 +37,7 @@ mongoClient.connect(url, {}, (error, client) => {
     })
 
     app.post('/users-integrators', (req, res) => {
-      const currentUser = req.body.currentUser;
-      let userIntegrators = [];
-      db.collection('users').find({
-        email: currentUser
-      }).toArray((error, result) => {
-        if (error) {
-          res.send("Nie udało się znależć użytkownika")
-          console.log("Nie udało się znależć użytkownika", error)
-        } else {
-          if (result.length === 1) {
-            // Ta część kodu sprawdza czy aktualnie zalogowany użytkownik posiada integrator ShopGold-AMPPolska. Jeżeli tak to dodaje jego nazwę do tablicy userIntegrators.
-            db.collection('ampPolska').find({
-              userID: result[0]._id
-            }).toArray((error, result) => {
-              if (error) {
-                res.send("Coś poszło nie tak")
-                console.log("Coś poszło nie tak", error)
-              } else {
-                if (result.length === 1) {
-                  userIntegrators.push("ShopGold-AMPPolska")
-                  res.send(userIntegrators)
-                }
-              }
-            })
-            // Ta część kodu sprawdza czy aktualnie zalogowany użytkownik posiada integrator ShopGold-FHSahs. Jeżeli tak to dodaje jego nazwę do tablicy userIntegrators.
-            db.collection('fhSahs').find({
-              userID: result[0]._id
-            }).toArray((error, result) => {
-              if (error) {
-                res.send("Coś poszło nie tak")
-                console.log("Coś poszło nie tak", error)
-              } else {
-                if (result.length === 1) {
-                  userIntegrators.push("ShopGold-FHSahs")
-                  res.send(userIntegrators)
-                }
-              }
-            })
-          }
-        }
-      })
+      usersIntegrators(req, res, db);
     })
 
     app.post('/shopgold-amppolska', (req, res) => {
